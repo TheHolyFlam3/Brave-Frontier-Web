8a8acad10542ca774f7715fceb80b093cb66279b
TheHolyFlam3 <theholyflam3nl@gmail.com>
2015-09-28 12:49:29 +0200

    Start Github
    1st

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brave Frontier Web</title>
+	<link rel="stylesheet" type="text/css" href="bfweb.css">
+	<link rel="icon" href="images/icon.png">
+</head>
+<body>
+	hello world
+</body>
+</html>
